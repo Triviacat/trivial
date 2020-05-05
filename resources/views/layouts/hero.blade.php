1e2e5d645455c0a83b5d6a27ad73f23d8bf4f207
robertgarrigos <robert@garrigos.cat>
2020-05-05 19:51:32 +0200

    cleanning

--- resources/views/layouts/hero.blade.php
+++ resources/views/layouts/hero.blade.php
@@ -21,14 +21,8 @@
     <meta name="twitter:description" content="El trivial en línia, obert, col·laboratiu i en català." />
     <meta name="twitter:site" content="{{ env('APP_URL') }}" />
     <title>{{ config('app.name', 'Laravel') }}</title>
-    {{-- <link rel="shortcut icon" href="../images/fav_icon.png" type="image/x-icon"> --}}
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-    {{-- <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> --}}
-    <!-- Bulma Version 0.8.x-->
-    {{-- <link rel="stylesheet" href="https://unpkg.com/bulma@0.8.0/css/bulma.min.css" /> --}}
-    {{-- <link rel="stylesheet" type="text/css" href="../css/hero.css"> --}}
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    {{-- <link rel="stylesheet" href="https://unpkg.com/bulma-modal-fx/dist/css/modal-fx.min.css" /> --}}
 </head>
 
 <body>
@@ -49,13 +43,16 @@
                             <div class="navbar-start"></div>
                             <div class="navbar-end">
                                 <a class="navbar-item" href="/games">Jugar</a>
-                                <a class="navbar-item" href="{{ env('TRIVIACAT_DOCS_URL') }}">@lang('trivial.instructions')&nbsp;<i class="fas fa-external-link-alt"></i></a>
+                                <a class="navbar-item"
+                                    href="{{ env('TRIVIACAT_DOCS_URL') }}">@lang('trivial.instructions')&nbsp;<i
+                                        class="fas fa-external-link-alt"></i></a>
 
-                                <a class="navbar-item" href="{{ env('TRIVIACAT_PREGUNTES_URL') }}">@lang('trivial.questions')&nbsp;<i class="fas fa-external-link-alt"></i></a>
+                                <a class="navbar-item"
+                                    href="{{ env('TRIVIACAT_PREGUNTES_URL') }}">@lang('trivial.questions')&nbsp;<i
+                                        class="fas fa-external-link-alt"></i></a>
 
                                 <span class="navbar-item">
-                                    <a class="button is-white is-outlined"
-                                        href="https://github.com/Triviacat/web">
+                                    <a class="button is-white is-outlined" href="https://github.com/Triviacat/web">
                                         <span class="icon">
                                             <i class="fab fa-github"></i>
                                         </span>
@@ -79,28 +76,24 @@
         </section>
         <div class="box cta">
             <p class="has-text-centered">
-                <span class="tag is-primary">Novetat!</span> Ja podeu <a href="/beta">provar la versió Beta</a> del joc en línia.
+                <span class="tag is-primary">Novetat!</span> Ja podeu <a href="/beta">provar la versió Beta</a> del joc
+                en línia.
             </p>
         </div>
         @if (session('message'))
-    <div class="notification  is-warning has-text-centered">
-        {{ session('message') }}
-    </div>
-@endif
-
-
-
+        <div class="notification  is-warning has-text-centered">
+            {{ session('message') }}
+        </div>
+        @endif
         <section class="container">
-            {{-- @dump(Auth()->user()); --}}
+
             @if (!isset(Auth()->user()->email_verified_at) && null !== (Auth()->user()))
-                <div class="notification  is-warning has-text-centered">Us hem enviat un correu de verificació amb un enllaç per finalitzar el procés de registre.</div>
+            <div class="notification  is-warning has-text-centered">Us hem enviat un correu de verificació amb un enllaç
+                per finalitzar el procés de registre.</div>
             @endif
             <div class="columns features">
                 <div class="column is-4">
                     <div class="card is-shady">
-                        {{-- <div class="card-image has-text-centered">
-                            <i class="fas fa-paw"></i>
-                        </div> --}}
                         <div class="card-content">
                             <div class="content">
                                 <h4>Instruccions</h4>
@@ -113,9 +106,6 @@
                 </div>
                 <div class="column is-4">
                     <div class="card is-shady">
-                        {{-- <div class="card-image has-text-centered">
-                            <i class="fab fa-empire"></i>
-                        </div> --}}
                         <div class="card-content">
                             <div class="content">
                                 <h4>Jugar</h4>
@@ -127,14 +117,12 @@
                 </div>
                 <div class="column is-4">
                     <div class="card is-shady">
-                        {{-- <div class="card-image has-text-centered">
-                            <i class="fab fa-apple"></i>
-                        </div> --}}
                         <div class="card-content">
                             <div class="content">
                                 <h4>Col·laborar</h4>
                                 <p>TriviaCat és un projecte obert i col·aboratiu. Podeu aportar les vostres pròpies <a
-                                        href="{{ env('TRIVIACAT_PREGUNTES_URL') }}">preguntes i respostes</a>. També podeu
+                                        href="{{ env('TRIVIACAT_PREGUNTES_URL') }}">preguntes i respostes</a>. També
+                                    podeu
                                     col·laborar en el desenvolupament i millora del codi que trobareu a <a
                                         href="https://github.com/Triviacat/web">Github</a>.</p>
                             </div>
@@ -142,194 +130,14 @@
                     </div>
                 </div>
             </div>
-            {{-- <div class="intro column is-8 is-offset-2">
-            <h2 class="title">Perfect for developers or designers!</h2><br>
-            <p class="subtitle">Vel fringilla est ullamcorper eget nulla facilisi. Nulla facilisi nullam vehicula ipsum
-                a. Neque egestas congue quisque egestas diam in arcu cursus.</p>
-        </div> --}}
-            {{-- <div class="sandbox">
-            <div class="tile is-ancestor">
-                <div class="tile is-parent is-shady">
-                    <article class="tile is-child notification is-white">
-                        <p class="title">Hello World</p>
-                        <p class="subtitle">What is up?</p>
-                    </article>
-                </div>
-                <div class="tile is-parent is-shady">
-                    <article class="tile is-child notification is-white">
-                        <p class="title">Foo</p>
-                        <p class="subtitle">Bar</p>
-                    </article>
-                </div>
-                <div class="tile is-parent is-shady">
-                    <article class="tile is-child notification is-white">
-                        <p class="title">Third column</p>
-                        <p class="subtitle">With some content</p>
-                        <div class="content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu
-                                pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis
-                                feugiat facilisis.</p>
-                        </div>
-                    </article>
-                </div>
-            </div>
-            <div class="tile is-ancestor">
-                <div class="tile is-vertical is-8">
-                    <div class="tile">
-                        <div class="tile is-parent is-vertical">
-                            <article class="tile is-child notification is-white">
-                                <p class="title">Vertical tiles</p>
-                                <p class="subtitle">Top box</p>
-                            </article>
-                            <article class="tile is-child notification is-white">
-                                <p class="title">Vertical tiles</p>
-                                <p class="subtitle">Bottom box</p>
-                            </article>
-                        </div>
-                        <div class="tile is-parent">
-                            <article class="tile is-child notification is-white">
-                                <p class="title">Middle box</p>
-                                <p class="subtitle">With an image</p>
-                                <figure class="image is-4by3">
-                                    <img src="https://picsum.photos/640/480/?random" alt="Description">
-                                </figure>
-                            </article>
-                        </div>
-                    </div>
-                    <div class="tile is-parent is-shady">
-                        <article class="tile is-child notification is-white">
-                            <p class="title">Wide column</p>
-                            <p class="subtitle">Aligned with the right column</p>
-                            <div class="content">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu
-                                    pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis
-                                    feugiat facilisis.</p>
-                            </div>
-                        </article>
-                    </div>
-                </div>
-                <div class="tile is-parent is-shady">
-                    <article class="tile is-child notification is-white">
-                        <div class="content">
-                            <p class="title">Tall column</p>
-                            <p class="subtitle">With even more content</p>
-                            <div class="content">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat
-                                    pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat
-                                    gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula
-                                    eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
-                                <p>Suspendisse varius ligula in molestie lacinia. Maecenas varius eget ligula a
-                                    sagittis. Pellentesque interdum, nisl nec interdum maximus, augue diam porttitor
-                                    lorem, et sollicitudin felis neque sit amet erat. Maecenas imperdiet
-                                    felis nisi, fringilla luctus felis hendrerit sit amet. Aenean vitae gravida diam,
-                                    finibus dignissim turpis. Sed eget varius ligula, at volutpat tortor.</p>
-                                <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue
-                                    lectus dolor consequat libero. Donec leo ligula, maximus et pellentesque sed,
-                                    gravida a metus. Cras ullamcorper a nunc ac porta. Aliquam
-                                    ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
-                            </div>
-                        </div>
-                    </article>
-                </div>
-            </div>
-            <div class="tile is-ancestor">
-                <div class="tile is-parent is-shady">
-                    <article class="tile is-child notification is-white">
-                        <p class="title">Side column</p>
-                        <p class="subtitle">With some content</p>
-                        <div class="content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu
-                                pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis
-                                feugiat facilisis.</p>
-                        </div>
-                    </article>
-                </div>
-                <div class="tile is-parent is-8 is-shady">
-                    <article class="tile is-child notification is-white">
-                        <p class="title">Main column</p>
-                        <p class="subtitle">With some content</p>
-                        <div class="content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu
-                                pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis
-                                feugiat facilisis.</p>
-                        </div>
-                    </article>
-                </div>
-            </div>
-            <div class="tile is-ancestor">
-                <div class="tile is-parent is-8 is-shady">
-                    <article class="tile is-child notification is-white">
-                        <p class="title">Murphy's law</p>
-                        <p class="subtitle">Anything that can go wrong will go wrong</p>
-                        <div class="content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu
-                                pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis
-                                feugiat facilisis.</p>
-                        </div>
-                    </article>
-                </div>
-                <div class="tile is-parent is-shady">
-                    <article class="tile is-child notification is-white">
-                        <p class="title">Main column</p>
-                        <p class="subtitle">With some content</p>
-                        <div class="content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu
-                                pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis
-                                feugiat facilisis.</p>
-                        </div>
-                    </article>
-                </div>
-            </div>
-        </div> --}}
         </section>
         <footer class="footer">
             <div class="container">
-                {{-- <div class="columns">
-                <div class="column is-3 is-offset-2">
-                    <h2><strong>Category</strong></h2>
-                    <ul>
-                        <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-                        <li><a href="#">Vestibulum errato isse</a></li>
-                        <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-                        <li><a href="#">Aisia caisia</a></li>
-                        <li><a href="#">Murphy's law</a></li>
-                        <li><a href="#">Flimsy Lavenrock</a></li>
-                        <li><a href="#">Maven Mousie Lavender</a></li>
-                    </ul>
-                </div>
-                <div class="column is-3">
-                    <h2><strong>Category</strong></h2>
-                    <ul>
-                        <li><a href="#">Labore et dolore magna aliqua</a></li>
-                        <li><a href="#">Kanban airis sum eschelor</a></li>
-                        <li><a href="#">Modular modern free</a></li>
-                        <li><a href="#">The king of clubs</a></li>
-                        <li><a href="#">The Discovery Dissipation</a></li>
-                        <li><a href="#">Course Correction</a></li>
-                        <li><a href="#">Better Angels</a></li>
-                    </ul>
-                </div>
-                <div class="column is-4">
-                    <h2><strong>Category</strong></h2>
-                    <ul>
-                        <li><a href="#">Objects in space</a></li>
-                        <li><a href="#">Playing cards with coyote</a></li>
-                        <li><a href="#">Goodbye Yellow Brick Road</a></li>
-                        <li><a href="#">The Garden of Forking Paths</a></li>
-                        <li><a href="#">Future Shock</a></li>
-                    </ul>
-                </div>
-            </div> --}}
                 <div class="content has-text-centered">
-                    {{-- <p>
-                    <a class="icon" href="https://github.com/Triviacat/web">
-                        <i class="fab fa-github"></i>
-                    </a>
-                </p> --}}
                     <div class="control level-item">
                         <a href="https://github.com/Triviacat/web">
                             <div class="tags has-addons">
-                                <span class="tag is-dark">TriviCat</span>
+                                <span class="tag is-dark">TriviaCat</span>
                                 <span class="tag is-primary">MIT license</span>
                             </div>
                         </a>
@@ -343,7 +151,6 @@
         window.default_locale = "{{ config('app.locale') }}";
         window.fallback_locale = "{{ config('app.fallback_locale') }}";
         window.messages = @json($messages);
-
     </script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
