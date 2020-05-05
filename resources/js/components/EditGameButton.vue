044b1fe38910ec88949ba253553baa78ca87151b
robertgarrigos <robert@garrigos.cat>
2020-05-05 12:30:23 +0200

    vuetified edit game buttom

---
+++ resources/js/components/EditGameButton.vue
@@ -0,0 +1,47 @@
+<template>
+    <a :href="url" class="button is-light is-small" v-show="isButtonShown">{{ trans.get('trivial.edit') }}</a>
+</template>
+
+
+<script>
+    export default {
+        props: {
+            game: {
+                type: Object
+            }
+        },
+        methods: {
+            // updateStatus: function (status) {
+            //     return this.trans.get('trivial.' + status);
+            // },
+
+            showButton: function (status) {
+                // console.log(this.game);
+                if (status == 'closed' || status == 'over') {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        },
+        data() {
+            return {
+                url: "/games/" + this.game.id + "/edit",
+                isButtonShown: this.showButton(this.game.status)
+            }
+        },
+        mounted() {
+            // console.log(this.game);
+        },
+        created() {
+            // console.log(this.game);
+            window.Echo.channel('game.' + this.game.id)
+                .listen('GameStatusHasChanged', e => {
+                    // console.log(e);
+                    this.isButtonShown = this.showButton(e.game.status);
+                })
+
+        }
+    };
+</script>
--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -53,9 +53,10 @@
                 {{-- @if ($game->status != 'started' && $game->status != 'over' && $game->status != 'closed')
                 <a href="/games/{{ $game->id }}/start" class="button is-success is-small">@lang('trivial.doStart')</a>
                 @endif --}}
-                @if ($game->status != 'over' && $game->status != 'closed')
+                <edit-game-button :game="{{ $game }}"></edit-game-button>
+                {{-- @if ($game->status != 'over' && $game->status != 'closed')
                 <a href="/games/{{ $game->id }}/edit" class="button is-light is-small">@lang('trivial.edit')</a>
-                @endif
+                @endif --}}
 
                 <form method="post" action="/games/{{ $game->id }}" style="display: inline-block;"
                     onsubmit="return confirm('Do you really want to delete?');">
