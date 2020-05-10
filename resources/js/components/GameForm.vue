bfa51e6e6bd562c4b2e665db79ae75a9bc7e28ef
robertgarrigos <robert@garrigos.cat>
2020-05-10 17:17:42 +0200

    create game translations

--- resources/js/components/GameForm.vue
+++ resources/js/components/GameForm.vue
@@ -1,29 +1,28 @@
-
 <template>
     <section>
         <div class="field">
             <b-radio v-model="radio" name="private" native-value="1">
-                Privat
+                {{ this.trans.get('trivial.privacityTrue') }}
             </b-radio>
         </div>
         <div class="field">
             <b-radio v-model="radio" name="private" native-value="0">
-                Públic
+                {{ this.trans.get('trivial.privacityFalse') }}
             </b-radio>
         </div>
-        <b-field label="URL Xat" message="Missatge d'ajuda">
+        <b-field :label="this.trans.get('trivial.chatUrl')" :message="this.trans.get('trivial.chatUrlHelp')">
             <b-input v-model="chat" name="chat"></b-input>
         </b-field>
-        <b-field label="Contrasenya Xat" message="Missatge d'ajuda">
+        <b-field :label="this.trans.get('trivial.chatPassword')" :message="this.trans.get('trivial.chatPasswordHelp')">
             <b-input v-model="password" name="password"></b-input>
         </b-field>
         <div class="field" v-show="showInvited">
-            <span class="label">Usuaris i usuàries que convides</span>
+            <span class="label">{{ this.trans.get('trivial.invitedUsers') }}</span>
             <user-multiselect v-model="selectedUsers" id="ajax" label="name" track-by="id"
-                placeholder="Escriu per començar a buscar" open-direction="bottom" :options="users" :multiple="true"
-                :searchable="true" :loading="isLoading" :internal-search="false" :clear-on-select="true"
-                :close-on-select="false" :options-limit="300" :limit="6" :max-height="600" :show-no-results="false"
-                :hide-selected="true" @search-change="asyncFind" @blur="notLoading">
+                :placeholder="this.trans.get('trivial.invitedUsers')" open-direction="bottom" :options="users"
+                :multiple="true" :searchable="true" :loading="isLoading" :internal-search="false"
+                :clear-on-select="true" :close-on-select="false" :options-limit="300" :limit="6" :max-height="600"
+                :show-no-results="false" :hide-selected="true" @search-change="asyncFind" @blur="notLoading">
                 <template slot="tag" slot-scope="{ option, remove }"><span
                         class="custom__tag"><span>{{ option.name }}</span><span class="custom__remove"
                             @click="remove(option)"><i class="far fa-times-circle"></i></span></span></template>
@@ -32,17 +31,13 @@
                         @mousedown.prevent.stop="clearAll(props.search)"></div>
                 </template><span slot="noResult">Oops! No elements found. Consider changing the search query.</span>
             </user-multiselect>
-            <p class="help">Pots convidar un màxim de sis persones</p>
-
-
+            <p class="help">{{ this.trans.get('trivial.invitedUsersHelp') }}</p>
         </div>
         <div class="field">
-            <button class="button">Enviar</button>
+            <button class="button">{{ this.trans.get('trivial.send') }}</button>
         </div>
         <input type="hidden" name="invited" :value="invited">
         <input type="hidden" name="user_id" :value="user_id">
-
-        <!-- <b-button @click="send">Enviar</b-button> -->
     </section>
 </template>
 
